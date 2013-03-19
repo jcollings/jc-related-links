2a216801357255e8768f580af083eac8ea942355
James Collings <james@jclabs.co.uk>
2013-03-19 16:35:06 +0000

    retrieve post relation

--- related-links.funcs.php
+++ related-links.funcs.php
@@ -28,4 +28,78 @@ function list_related_posts()
 	return $output;
 }
 
+/**
+ * Get Specific Post Type Relation
+ * @return array id
+ */
+function get_related_post_type($post_type = '')
+{
+	global $RelatedLinks;
+	$post_id = get_the_ID();
+
+	$current_post_type = get_post_type();
+	$options = $RelatedLinks->_core->get_options();
+	$output = array();
+
+	if(empty($post_type)){
+		$active_post_types = array_keys($options[$current_post_type.'_type'], 'true');
+		foreach($active_post_types as $value){
+			if($options[$value.'_type'][$current_post_type] == 1){
+				$q = new WP_Query(array(
+					'post_type' => $value
+				));
+
+				while($q->have_posts()){
+					$q->the_post();
+					$test = get_post_meta( get_the_id(), 'related_service_page', true);
+					
+					if(in_array($post_id, $test)){
+						$output[] = array(
+							'id' => get_the_ID(),
+							'count' => count($test)
+						);
+					}
+				}
+
+				wp_reset_postdata();
+			}
+		}
+	}else{
+		if($options[$post_type.'_type'][$current_post_type] == 1){
+			$q = new WP_Query(array(
+				'post_type' => $post_type
+			));
+
+			while($q->have_posts()){
+				$q->the_post();
+				$test = get_post_meta( get_the_id(), 'related_service_page', true);
+				
+				if(in_array($post_id, $test)){
+					$output[] = array(
+						'id' => get_the_ID(),
+						'count' => count($test)
+					);
+				}
+			}
+
+			wp_reset_postdata();
+		}
+	}
+
+	if(empty($output))
+		return false;
+
+	$selected = null;
+	foreach($output as $o){
+		if(is_array($selected)){
+			if($o['count'] < $selected['count']){
+				$selected = $o;
+			}
+		}else{
+			$selected = $o;
+		}
+	}
+	
+	return $selected;
+}
 ?>
